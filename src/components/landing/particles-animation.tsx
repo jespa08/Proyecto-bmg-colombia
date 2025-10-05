110c1d6942745f99205b1f4cd0addf96d2553179
Jessica Salazar <jessicaaljure@gmail.com>
2025-10-05 20:52:59 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@t

--- src/components/landing/particles-animation.tsx
+++ src/components/landing/particles-animation.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useMemo, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
+import Particles, { initParticlesEngine } from "react-tsparticles";
 import {
   type Container,
   type ISourceOptions,
